6ff96a96a481a672183fdcb48f620d3d944f5bc4
Bruno <Bruno_lg_@hotmail.com>
2022-07-13 21:13:34 -0300

    experimentos e tratamento de erro

---
+++ exercicios-js/fundamentos/erro.js
@@ -0,0 +1,25 @@
+function tratarerroelancar(erro) {
+   // throw new Error('...')
+   // throw 10
+   // throw true
+   // throw 'mensagem'
+   throw {
+        nome: erro.name,
+        msg: erro.messege,
+        date: new Date
+   }
+}
+
+function imprimirnomegritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarerroelancar(e)
+    } finally {
+        console.log('final')
+    }
+    
+}
+
+const obj = { nome: 'Roberto' }
+imprimirnomegritado(obj) 
