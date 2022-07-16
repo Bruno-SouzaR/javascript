38f94ba7e23805e6641e61918a424bcdb727c8c6
Bruno <Bruno_lg_@hotmail.com>
2022-07-16 15:14:19 -0300

    Create factury2.js

---
+++ exercicios-js/funcao/factury2.js
@@ -0,0 +1,11 @@
+function criarproduto(nome, preco) {
+    return {
+    nome,
+    preco,
+    desconto: 0.1
+
+}
+}
+
+console.log(criarproduto('iPad', 1199.49))
+console.log(criarproduto('Notebook', 2199.49))
