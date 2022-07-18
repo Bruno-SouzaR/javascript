d3fc811d2b6244cb94bea910757c0a9236c44662
Bruno <Bruno_lg_@hotmail.com>
2022-07-17 22:36:51 -0300

    objeto

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,23 @@
+// Cadigo nao executavel
+
+// Procedual
+processamento(valor1, valor2, valor3)
+
+// OO
+
+Object = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. heranca (prototype)
+// 4. polimorfismo
