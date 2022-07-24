388640bc23af6d8a08774bcc1f20edf1fc1ca4f6
Bruno <Bruno_lg_@hotmail.com>
2022-07-24 08:27:10 -0300

    modulos

---
+++ exercicios-js/node/moduloa.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala pessoal'
+exports.bemvindo = 'Bem vindo ao node!'
+module.exports.atelogo = 'Ate o proximo exemplo'
---
+++ exercicios-js/node/modulocliente.js
@@ -0,0 +1,10 @@
+const moduloa = require('./moduloa')
+const modulob = require('./modulob')
+
+console.log(moduloa.ola)
+console.log(moduloa.bemvindo)
+console.log(moduloa.atelogo)
+console.log(moduloa)
+console.log(modulob.bomdia)
+console.log(modulob.boanoite())
+console.log(modulob)
