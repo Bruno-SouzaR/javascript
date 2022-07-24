90a60f18f50f32ef2a7933c5d5c70b03b9f2206e
Bruno <Bruno_lg_@hotmail.com>
2022-07-24 08:27:35 -0300

    Update modulocliente.js

--- exercicios-js/node/modulocliente.js
+++ exercicios-js/node/modulocliente.js
@@ -5,6 +5,7 @@ console.log(moduloa.ola)
 console.log(moduloa.bemvindo)
 console.log(moduloa.atelogo)
 console.log(moduloa)
+
 console.log(modulob.bomdia)
 console.log(modulob.boanoite())
 console.log(modulob)
