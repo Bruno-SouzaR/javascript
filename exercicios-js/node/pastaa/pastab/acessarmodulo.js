ea7a46a0d55ca94eb52182d639ecc14987e7a8d2
Bruno <Bruno_lg_@hotmail.com>
2022-07-26 14:33:04 -0300

    exemplos de require

---
+++ exercicios-js/node/pastaa/pastab/acessarmodulo.js
@@ -0,0 +1,11 @@
+const moduloa = require('../../moduloa')
+console.log(moduloa.ola)
+
+const c = require('./pastac')
+console.log(c.ola2)
+
+//  const http = require('http')
+//  http.createServer((req, res) => {
+//     res.write('Bom dia!')
+//     res.end()    
+// }).listen(8080)
