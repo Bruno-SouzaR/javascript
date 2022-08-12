06f71ca2229111352ef82f8ba47d8b8fc6867b34
Bruno <Bruno_lg_@hotmail.com>
2022-08-12 10:48:51 -0300

    Create consultarcomjoin.sql

---
+++ exercicios-js/banco-relacional/consultarcomjoin.sql
@@ -0,0 +1,13 @@
+select 
+    e.nome as estados,
+    c.nome as cidades,
+    regiao as regiao
+from estados e, cidades c
+where e.id = c.estado_id;
+
+select
+    c.nome as cidade,
+    e.nome as estado,
+    regiao as regiao
+from estados e 
+inner join cidades c on e.id = c.estado_id
